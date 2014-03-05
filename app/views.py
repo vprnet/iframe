d1c9d2eb3da3a23bb7640a615deb1db6e932ee39
Matt Parrilla <matthew.parrilla@gmail.com>
2014-03-05 12:40:06 -0500

    Update README, create town-meeting dir

--- app/views.py
+++ app/views.py
@@ -31,27 +31,27 @@ def broadband():
     return render_template('broadband.html')
 
 
-@app.route('/town-meeting-school-budget')
+@app.route('/town-meeting/school-budget')
 def school_budget():
     return render_template('school-budget.html')
 
 
-@app.route('/town-meeting-town-budget')
+@app.route('/town-meeting/town-budget')
 def town_budget():
     return render_template('town-budget.html')
 
 
-@app.route('/town-meeting-public-bank')
+@app.route('/town-meeting/public-bank')
 def public_bank():
     return render_template('public-bank.html')
 
 
-@app.route('/town-meeting-results')
+@app.route('/town-meeting/results')
 def results():
     return render_template('results.html')
 
 
-@app.route('/town-meeting-tar-sands')
+@app.route('/town-meeting/tar-sands')
 def tar_sands():
     return render_template('tar-sands.html')
 
